9851d84a86d5461f2b30d15d62354b6a5c24e95c
Michaela McKanna <michaela.mckanna@gmail.com>
2021-08-04 04:30:45 -0500

    fix mobile responsiveness

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -11,6 +11,7 @@ const About = () => {
          <div className="about" >
             <div className="text">
                 <p>You can count on the vocals of Scott Barnett on his Shure Super 55 mic to get the crowd jumping. Jon "Mother Fucking" Carter brings a certain style of guitar licks unmatched in KC. Tyler Miller on drums keeps the music moving with energy that they didn't have before. Drek has proved to be one band that will continue to put on a great show and bring strong live performances leaving new and current fans wanting more! Some people may tag Drek as a party band, but don’t take that lightly. Party is what they are all about, but music is what they make and deliver. They bring straight up talent to the industry of Party Hard Rock. 12 years in the business of making music will tell the tale. They continue to make their way thru the saturated music industry with a name that sticks in your mind, “Drek Nation” is the chant when they are urged to play one more, but count on them to play at least two. </p>
+                <br/>
             </div> 
         </div>
         <div className="images">
